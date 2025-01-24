b4f72e6ffc56f8d805f2c0edf394dcdaba4550d6
kwonws <kevincool6@naver.com>
2025-01-24 21:34:34 +0900

    Initial Commit

---
+++ WatchBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchBox
+//
+//  Created by 권우석 on 1/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
