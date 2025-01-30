a301abe02112074c0dc2220f274f9ea10c0c06ba
kwonws <kevincool6@naver.com>
2025-01-31 01:26:17 +0900

    검색결과화면 구현

---
+++ WatchBox/DataModels/Genre.swift
@@ -0,0 +1,8 @@
+//
+//  Genre.swift
+//  WatchBox
+//
+//  Created by 권우석 on 1/30/25.
+//
+
+import Foundation
---
+++ WatchBox/Main_FirstTab/SearchKeywordCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchKeywordCollectionViewCell.swift
+//  WatchBox
+//
+//  Created by 권우석 on 1/30/25.
+//
+
+import UIKit
+
+class SearchKeywordCollectionViewCell: UICollectionViewCell {
+    
+}
